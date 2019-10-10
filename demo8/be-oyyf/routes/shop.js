699875c82863f4cf925c63a8ff55c6e400532218
oyyf1123 <o18879966669@163.com>
2019-10-10 20:33:04 +0800

    后端1

---
+++ demo8/be-oyyf/routes/shop.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.route('/shop')
+  .post((res,req,next) => {
+    
+  })
+  .put()
+  .delete()
+  .get()
+
+module.exports = router
